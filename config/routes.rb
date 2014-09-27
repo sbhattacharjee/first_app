eb009bac2acb60517ce9d19fa2e5f5f8c272f5d2
sbhattacharjee <bhattacharjee.sudarshan@gmail.com>
2014-09-27 21:41:27 +0530

    Adding assets - CSS/JS/Image, structuring home page, defining route_path and simplifying Rspec settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 FirstApp::Application.routes.draw do
-  get "static_page/home"
-  get "static_page/help"
-  get "static_page/about"
-  get "static_page/contact"
+  root  'static_page#home'
+  match '/help',    to: 'static_page#help',    via: 'get'
+  match '/about',   to: 'static_page#about',   via: 'get'
+  match '/contact', to: 'static_page#contact', via: 'get'
   resources :microposts
 
   resources :users
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,57 +2,82 @@ require 'spec_helper'
 
 describe "Static page" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }	
+  subject { page }
+
+  # let(:base_title) { "Ruby on Rails Tutorial Sample App" }	
   
   describe "Home page" do
 
-    it "should have the content 'First App'" do
-      visit '/static_page/home'
-      expect(page).to have_content('First App')
-    end
+    before { visit root_path }
 
-    it "should have the title 'Home'" do
-      visit '/static_page/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
+    it { should have_content('First App') }
+    it { should have_title( full_title('Home') ) }
+
+  
+    #  it "should have the content 'First App'" do
+    #    visit root_path
+    #    expect(page).to have_content('First App')
+    #  end
+
+    #  it "should have the title 'Home'" do
+    #   visit root_path
+    #    expect(page).to have_title("#{base_title} | Home")
+    #  end
+    
+
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_page/help'
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_page/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+
+    #  it "should have the content 'Help'" do
+    #    visit help_path
+    #    expect(page).to have_content('Help')
+    #  end
+
+    #  it "should have the title 'Help'" do
+    #    visit help_path
+    #    expect(page).to have_title("#{base_title} | Help")
+    #  end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_page/about'
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit '/static_page/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
+
+    #  it "should have the content 'About Us'" do
+    #   visit about_path
+    #    expect(page).to have_content('About Us')
+    #  end
+
+    #  it "should have the title 'About Us'" do
+    #    visit about_path
+    #    expect(page).to have_title("#{base_title} | About Us")
+    #  end
   end
 
 	describe "Contact page" do
+    before { visit contact_path }
 
-		it "should have the content 'Contact'" do
-			visit '/static_page/contact'
-			expect(page).to have_content('Contact')
-		end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
 
-		it "should have the title 'Contact'" do
-			visit '/static_page/contact'
-			expect(page).to have_title("#{base_title}  | Contact")
-		end
+  	#	it "should have the content 'Contact'" do
+  	#		visit contact_path
+  	#		expect(page).to have_content('Contact')
+  	#	end
+
+  	#	it "should have the title 'Contact'" do
+  	#		visit contact_path
+  	#		expect(page).to have_title("#{base_title}  | Contact")
+  	#	end
 	end
-end
+
+end    # Static page ends here
